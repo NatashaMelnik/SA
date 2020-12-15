fb0e6d4617d3a3a7c299b1383c35306d737a4d02
NatashaMelnik <n.melnik1788@gmail.com>
2020-12-15 21:29:29 +0200

    lr2

---
+++ lr2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lr2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        public void calculate()
+        {
+            double k1 = GetNumber(textBox2.Text);
+            double k2 = GetNumber(textBox3.Text);
+            double Ak1 = GetNumber(textBox4.Text);
+            double Ak2 = GetNumber(textBox9.Text);
+            double Bk1 = GetNumber(textBox5.Text);
+            double Bk2 = GetNumber(textBox8.Text);
+            double Ck1 = GetNumber(textBox6.Text);
+            double Ck2 = GetNumber(textBox7.Text);
+
+            double A_res = k1 * Ak1 + k2 * Ak2;
+            double B_res = k1 * Bk1 + k2 * Bk2;
+            double C_res = k1 * Ck1 + k2 * Ck2;
+
+            label19.Text = $"A={A_res}";
+            label20.Text = $"A={B_res}";
+            label21.Text = $"A={C_res}";
+
+            double max_res = Math.Max(A_res, Math.Max(B_res, C_res));
+
+            string answer = $"Комбінована вага найбільша у параметра ";
+
+            if (max_res == A_res)
+            {
+                label19.BackColor = System.Drawing.Color.LightGreen;
+                answer = answer + "A";
+            }
+            if (max_res == B_res)
+            {
+                label20.BackColor = System.Drawing.Color.LightGreen;
+                answer = answer + "B";
+            }
+            if (max_res == C_res)
+            {
+                label21.BackColor = System.Drawing.Color.LightGreen;
+                answer = answer + "C";
+            }
+
+            MessageBox.Show(answer);
+        }
+
+        static double GetNumber(string str)
+        {
+            double number;
+            bool success = Double.TryParse(str, out number);
+
+            if (success)
+            {
+                return number;
+            }
+            else
+            {
+                MessageBox.Show("Enter Number!!!");
+                return 0;
+            }
+        }
+    }
+}
---
+++ lr2_severalCriteria/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace lr2_severalCriteria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество уровней критериев иерархии: ");
+            string countCriteria_str = Console.ReadLine();
+
+            double countCriteria = GetNumber(countCriteria_str);
+
+            Calculate(countCriteria);
+
+            Console.ReadKey();
+        }
+
+        static void Calculate(double countCriteria)
+        {
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            double[][] criteria = new double[(int)countCriteria][];
+            for (int i = 0; i < countCriteria; i++)
+            {
+                Console.Write($"Введите количество критериев {i+1}й иерархии: ");
+                string str = Console.ReadLine();
+                int count = (int)GetNumber(str);
+                criteria[i] = new double[count];
+                string test = Console.ReadLine();
+                string[] temp = test.Split(new Char[] { ' ' });
+                for (int j = 0; j < count; j++)
+                {
+                    criteria[i][j] = double.Parse(temp[j]);
+                }
+            }
+
+            double count_answ = criteria[criteria.Length-1].Length / criteria[criteria.Length - 2].Length;
+
+            double[] ans = new double[(int)count_answ];
+
+            for (int i = 0; i < count_answ; i++)
+            {
+                double res_ = 1;
+
+                for (int j = 0; j < criteria[criteria.Length - 2].Length; j=j+ (int)count_answ)
+                {
+                    for (int q = 0; q < criteria[criteria.Length - 3].Length; q++)
+                    {
+                        res_ = ans[i] * criteria[j][q];
+                    }
+                }
+                ans[i] = res_;
+            }
+            double[] ansDop = ans;
+            Array.Sort(ansDop);
+            int ind = Array.IndexOf(ans, ansDop[0]);
+            Console.WriteLine($"{alpha[ind]}");
+        }
+
+        static double GetNumber(string str)
+        {
+            double number;
+            bool success = Double.TryParse(str, out number);
+
+            if (success)
+            {
+                return number;
+            }
+            else
+            {
+                Console.WriteLine("Enter Number!!!");
+                return 0;
+            }
+        }
+    }
+}
